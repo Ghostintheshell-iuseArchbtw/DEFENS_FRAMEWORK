1a7f131e6d6324c0909426fa48e171e8fa901a3b
Ghostintheshell <111326458+Ghostintheshell-iuseArchbtw@users.noreply.github.com>
2023-12-21 06:31:59 +0000

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -3,6 +3,25 @@
 import socket
 import threading
 
+class Honeypot:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = None
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+
+    def send(self, data):
+        self.socket.sendall(data.encode())
+
+    def receive(self, buffer_size):
+        return self.socket.recv(buffer_size).decode()
+
+    def close(self):
+        self.socket.close()
+
 def handle_connection(client_socket, client_address):
     print(f'Connection attempt from: {client_address[0]}:{client_address[1]}')
     response = 'Welcome to the honeypot!\n'
